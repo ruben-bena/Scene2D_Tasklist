3db1d491503fb9638711b4d8bccc4ce579d4155d
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2026-04-08 18:08:39 +0200

    estado inicial del ejercicio con codigo bytes.cat

--- core/src/main/java/com/rubenbellido/scene2d_tasklist/Main.java
+++ core/src/main/java/com/rubenbellido/scene2d_tasklist/Main.java
@@ -1,32 +1,73 @@
 package com.rubenbellido.scene2d_tasklist;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
-
+    private Stage stage;
+    private Skin skin;
+    public FitViewport viewport;
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        // Crear un Stage i un Skin
+        viewport = new FitViewport(8,5);
+        stage = new Stage(viewport);
+        skin = new Skin(Gdx.files.internal("uiskin.json")); // Carregar un Skin per defecte
+        float escala = viewport.getWorldHeight() / Gdx.graphics.getHeight();
+
+        // Ajustem paràmetres per a la font
+        Label.LabelStyle labelStyle = new Label.LabelStyle();
+        labelStyle.font = new BitmapFont(); // Font per defecte
+        labelStyle.font.setUseIntegerPositions(false);
+        labelStyle.font.getData().setScale( escala );
+
+        // Creem label (TextView)
+        Label label = new Label("Hola, això és un TextView", labelStyle);
+        label.setPosition(1, 1); // Posició del Label
+
+        // Crear un Button
+        TextButton button = new TextButton("Clica el botonet!", skin );
+        button.setPosition(2, 2); // Posició del Button
+        button.setTransform(true);
+        button.setScale( 2*escala );
+        button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("Botó clicat!");
+            }
+        });
+
+        // Afegir els actors al Stage
+        stage.addActor(label);
+        stage.addActor(button);
+
+        // Configurar l'Stage com a gestor d'entrada
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        // Netejar la pantalla
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // Actualitzar i dibuixar l'Stage
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        // Alliberar recursos
+        stage.dispose();
+        skin.dispose();
     }
 }
