837a9d7368c96c628f58ed7e9e1c31cfd19edbe8
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2026-05-07 19:44:52 +0200

    Añadir lista de tareas basica combinando array,list,scrollpane

--- core/src/main/java/com/rubenbellido/scene2d_tasklist/Main.java
+++ core/src/main/java/com/rubenbellido/scene2d_tasklist/Main.java
@@ -2,17 +2,24 @@ package com.rubenbellido.scene2d_tasklist;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+import java.util.ArrayList;
 
 public class Main extends ApplicationAdapter {
     private Stage stage;
@@ -21,7 +28,7 @@ public class Main extends ApplicationAdapter {
     @Override
     public void create() {
         // Crear un Stage i un Skin
-        viewport = new FitViewport(8,5);
+        viewport = new FitViewport(800,500);
         stage = new Stage(viewport);
         skin = new Skin(Gdx.files.internal("uiskin.json")); // Carregar un Skin per defecte
         float escala = viewport.getWorldHeight() / Gdx.graphics.getHeight();
@@ -33,12 +40,12 @@ public class Main extends ApplicationAdapter {
         labelStyle.font.getData().setScale( escala );
 
         // Creem label (TextView)
-        Label label = new Label("Hola, això és un TextView", labelStyle);
-        label.setPosition(1, 1); // Posició del Label
+        Label label = new Label("Hola, això és un TextView", skin);
+        label.setPosition(100, 100); // Posició del Label
 
         // Crear un Button
         TextButton button = new TextButton("Clica el botonet!", skin );
-        button.setPosition(2, 2); // Posició del Button
+        button.setPosition(100, 200); // Posició del Button
         button.setTransform(true);
         button.setScale( 2*escala );
         button.addListener(new ClickListener() {
@@ -55,12 +62,26 @@ public class Main extends ApplicationAdapter {
         textFieldStyle.font.getData().setScale( escala );
         textFieldStyle.fontColor = Color.CYAN;
         TextField textField = new TextField("prueba", textFieldStyle);
-        textField.setPosition(3, 3);
+        textField.setPosition(100, 300);
+
+        // Crear Array para guardar tareas
+        Array<String> tasks = new Array<>();
+        tasks.add("uno","dos","tres");
+
+        // Crear una List
+        List<String> list = new List<>(skin);
+        list.setItems(tasks);
+
+        // Crear un ScrollPane
+        ScrollPane scrollPane = new ScrollPane(list, skin);
+        scrollPane.setPosition(400, 0);
+        scrollPane.setSize(400, 500);
 
         // Afegir els actors al Stage
         stage.addActor(label);
         stage.addActor(button);
         stage.addActor(textField);
+        stage.addActor(scrollPane);
 
         // Configurar l'Stage com a gestor d'entrada
         Gdx.input.setInputProcessor(stage);
